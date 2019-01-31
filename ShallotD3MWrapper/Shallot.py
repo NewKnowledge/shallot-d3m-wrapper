894b3d37dcd618321e3944e9203f5140130d8e2f
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-01-30 21:48:25 -0600

    Update Shallot.py

--- ShallotD3MWrapper/Shallot.py
+++ ShallotD3MWrapper/Shallot.py
@@ -15,7 +15,7 @@ from d3m.container import DataFrame as d3m_DataFrame
 from d3m.metadata import hyperparams, base as metadata_base, params
 from common_primitives import utils as utils_cp, dataset_to_dataframe as DatasetToDataFrame
 
-from timeseries_loader import TimeSeriesLoaderPrimitive
+from .timeseries_loader import TimeSeriesLoaderPrimitive
 
 __author__ = 'Distil'
 __version__ = '1.0.1'
