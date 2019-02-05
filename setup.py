2993b608b75ba36c24cbbf89b976530069bbec32
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-05 14:26:51 -0600

    update sloth requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='ShallotD3MWrapper',
     description='A thin wrapper for interacting with New Knowledge shapelet learning tool Shallot',
     packages=['ShallotD3MWrapper'],
     install_requires=["typing",
-        "Sloth==2.0.3"],
+        "Sloth>=2.0.2"],
     dependency_links=[
         "git+https://github.com/NewKnowledge/sloth@8e2b856617780b9c1527929a0d0be34d81b272fa#egg=Sloth-2.0.3"
 
