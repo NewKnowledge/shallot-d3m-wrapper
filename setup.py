1d7c669bc9074ec54039061e58967028f2c4512d
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-05 13:40:15 -0600

    bump version of Sloth

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(name='ShallotD3MWrapper',
     description='A thin wrapper for interacting with New Knowledge shapelet learning tool Shallot',
     packages=['ShallotD3MWrapper'],
     install_requires=["typing",
-        "Sloth==2.0.2"],
+        "Sloth==2.0.3"],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/sloth@8e2b856617780b9c1527929a0d0be34d81b272fa#egg=Sloth-2.0.2"
+        "git+https://github.com/NewKnowledge/sloth@8e2b856617780b9c1527929a0d0be34d81b272fa#egg=Sloth-2.0.3"
 
     ],
     entry_points = {
